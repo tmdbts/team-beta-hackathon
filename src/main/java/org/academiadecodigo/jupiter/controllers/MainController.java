2d46d958f1bac07a5ee89581b11289113c60c9a5
oEdgarAlves <edgaralves@ua.pt>
2020-12-16 19:30:14 +0000

    Some Controllers Done

---
+++ src/main/java/org/academiadecodigo/jupiter/controllers/MainController.java
@@ -0,0 +1,14 @@
+package org.academiadecodigo.jupiter.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "redirect:/home";
+    }
+
+}
