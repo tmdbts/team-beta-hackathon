7a29e8a148da363ed48b868d433cdc7c6808815a
oEdgarAlves <edgaralves@ua.pt>
2020-12-17 19:50:27 +0000

    resando

--- src/main/java/org/academiadecodigo/jupiter/controllers/assembler/UserToDto.java
+++ src/main/java/org/academiadecodigo/jupiter/controllers/assembler/UserToDto.java
@@ -3,6 +3,9 @@ package org.academiadecodigo.jupiter.controllers.assembler;
 import org.academiadecodigo.jupiter.persistance.model.User;
 import org.academiadecodigo.jupiter.persistance.model.dto.UserDto;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class UserToDto {
 
     public UserDto convert(User user) {
@@ -17,5 +20,13 @@ public class UserToDto {
         userDto.setAddress(user.getAddress());
 
         return userDto;
+    }
+
+    public List<UserDto> convert(List<User> users){
+        List<UserDto> dtoList = new LinkedList<>();
+        for (User user: users){
+            dtoList.add(convert(user));
+        }
+        return dtoList;
     }
 }
